240e6a2eb2c7ba0915e3e41fd29cfa760b0b0fe7
Adam Pawluczuk <adam.pawluczuk@ppuc.pl>
2022-04-25 16:45:32 +0200

    initial commit

---
+++ src/main/java/com/zonner93/rickapp/RickAppApplication.java
@@ -0,0 +1,13 @@
+package com.zonner93.rickapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RickAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RickAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zonner93/rickapp/RickAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zonner93.rickapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RickAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
